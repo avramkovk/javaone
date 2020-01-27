1b89b1ddb9d50a6b772bda522d3bac3117c493bc
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-01-27 14:16:28 +0300

    тест1

---
+++ src/test/exampleLesson7/main/java/func1.java
@@ -0,0 +1,15 @@
+package test.exampleLesson7.main.java;
+
+public class func1 {
+
+    static String finc(){
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        String a = finc();
+        System.out.println(a);
+
+
+    }
+}
---
+++ src/test/exampleLesson7/main/java/rectProcedure.java
@@ -0,0 +1,19 @@
+package test.exampleLesson7.main.java;
+
+
+public class rectProcedure {
+
+    static void rect(int a){
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+rect(4); // а - высота прямоугольника
+
+    }
+}
