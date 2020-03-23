418be433e2a38a2afc40c464d6dd29cab5c52e69
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-03-23 16:50:51 +0300

    Thread_class3

---
+++ src/forTest/Class3.java
@@ -0,0 +1,37 @@
+package forTest;
+
+public class Class3 {
+    public static void main(String[] args) {
+        Thread.currentThread().setName("Thread_main");
+
+        OtherThread otherThread = new OtherThread();
+        Thread threadRunnable = new Thread(new MyRunnable());
+
+        threadRunnable.start();
+        otherThread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
+
+class OtherThread extends Thread {
+    @Override
+    public void run() {
+        Thread.currentThread().setName("Thread_otherTread");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        Thread.currentThread().setName("Thread_myRunnable");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
