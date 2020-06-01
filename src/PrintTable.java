c224f67810a78a986c76b438f95efc8bfe81dfcd
Konstantin Avramkov <avramkovk@yandex.ru>
2020-06-02 02:01:44 +0300

    универсальный вывод таблицы sql

--- src/PrintTable.java
+++ src/PrintTable.java
@@ -8,56 +8,38 @@ public class PrintTable {
 
     public static void main(String[] args) {
         selectAndPrintAll();
-
     }
 
     private static void selectAndPrintAll() {
         AsciiTable at = new AsciiTable();
-
-
-
-
-
         final String URL = "jdbc:postgresql://localhost/postgres?user=postgres&password=12345";
         try (Connection conn = DriverManager.getConnection(URL)) {
             Statement stmt1 = conn.createStatement();
             ResultSet rs1 = stmt1.executeQuery("SELECT name, model, year FROM cars");
             ResultSetMetaData rsmd = rs1.getMetaData();
             int count = rsmd.getColumnCount();
-            int sizeList = count-1;
-            System.out.println("count "+ count);
-            System.out.println("sizeList "+sizeList);
-
-            System.out.println(rsmd.getColumnName(1));
-            System.out.println(rsmd.getColumnName(2));
-            System.out.println(rsmd.getColumnName(3));
+            int sizeList = count - 1;
 
             List<String> list = new ArrayList<>();
             for (int j = 0; j <= sizeList; j++) {
-                list.add(rsmd.getColumnName(j+1));
+                list.add(rsmd.getColumnName(j + 1));
             }
 
-            StringBuilder sb = new StringBuilder();
-            sb.append("\"");
-            for (int i = 0; i < list.size(); i++) {
-                sb.append(list.get(i));
-                sb.append(",");
-            }
-            sb.append("\"");
-           String result = sb.replace(sb.length()-2,sb.length()-1,"").toString();
-            System.out.println(result);
-
-
             at.addRule();
-            at.addRow("name", "model", "year");
+            at.addRow(list);
             at.addRule();
+
             while (rs1.next()) {
-                String name = rs1.getString(1);
-                String model = rs1.getString(2);
-                int year = rs1.getInt(3);
-                at.addRow(name, model, year);
+                List<Object> listqq = new ArrayList<>();
+                for (int j = 0; j <= sizeList; j++) {
+                    if (rsmd.getColumnType(j + 1) == 12) {
+                        listqq.add(rs1.getString(j + 1));
+                    } else {
+                        listqq.add(rs1.getInt(j + 1));
+                    }
+                }
+                at.addRow(listqq);
             }
-
             rs1.close();
             stmt1.close();
         } catch (SQLException e) {
@@ -66,6 +48,5 @@ public class PrintTable {
         at.addRule();
         String rend = at.render();
         System.out.println(rend);
-
     }
 }
